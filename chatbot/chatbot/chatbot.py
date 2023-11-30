03310b8ff4dbb740caeac0647ebbb521d3877215
munibali21 <123536567+munibali21@users.noreply.github.com>
2023-11-30 18:31:58 +0500

    Add files via upload
    https://youtu.be/ON5pGUJDNow?si=wRPAPgav7GR9H8oM ye dekh kr bnao

---
+++ chatbot/chatbot/chatbot.py
@@ -0,0 +1,29 @@
+import tensorflow
+import random
+import json
+import nltk
+import numpy
+import tflearn
+from nltk.stem.lancaster import LancasterStemmer
+stemmer=LancasterStemmer()
+
+with open("intents.josn") as file:
+    data=json.load(file)
+    
+words=[]
+labels=[]
+docs_x=[]
+docs_y=[]
+
+for intent in data['intents']:
+    for pattern in intent['patterns']:
+        wrds=nltk.word_tokenize(pattern)
+        words.extend(wrds)
+        docs_x.append(pattern)
+        docs_y.append(intent['tag'])
+        
+        if intent['tag'] not in labels:
+            labels.append(intent["tag"])
+words=[stemmer.stem(w.lower())for w in words]
+words=sorted(list(set(words)))
+labels=sorted(labels)
